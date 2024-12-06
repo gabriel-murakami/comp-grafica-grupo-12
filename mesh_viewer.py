ccacf84b2cb40f20dff0c387a718e779a5f66e8a
Gabriel M. Alves <56936297+gabriel-murakami@users.noreply.github.com>
2024-12-06 20:25:37 -0300

    Create mesh_viewer.py

---
+++ mesh_viewer.py
@@ -0,0 +1,23 @@
+import sys
+import open3d as o3d
+
+class PLYRenderer:
+    def __init__(self, path):
+        self.path = path
+
+    def renderizar_ply(self):
+        try:
+            mesh = o3d.io.read_triangle_mesh(self.path)
+            if not mesh.is_empty():
+                o3d.visualization.draw_geometries([mesh])
+            else:
+               print("Erro", f"O arquivo {self.path} está vazio ou inválido.")
+        except Exception as e:
+           print("Erro", f"Falha ao carregar o arquivo {self.path}: {str(e)}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    renderer = PLYRenderer(sys.argv[1])
+    renderer.renderizar_ply()
